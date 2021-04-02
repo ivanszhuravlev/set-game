f0693a67bca6d7ae9639f8215888b07dd6ba54b2
izhuravlev <ivan.zhuravlev@noveogroup.com>
2021-04-02 11:15:46 +0300

    Initial Commit

---
+++ Play Set/Play_SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Play_SetApp.swift
+//  Play Set
+//
+//  Created by dtermined on 02.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct Play_SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
